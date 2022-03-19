a178c70487a32874c14c010b9703569eb5d0e448
vashnavichauhan1825 <70444778+vashnavichauhan1825@users.noreply.github.com>
2022-03-19 12:01:11 +0530

    add mockbee backend

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom";
 import App from './App';
 import '../node_modules/font-awesome/css/font-awesome.min.css'; 
+import { makeServer } from "./server";
 
-
-
+makeServer();
 
 ReactDOM.render(
   
